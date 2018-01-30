a6fbc20b9a4e115b7801ca1f5706810aa3b9772e
Yauhen_Kavalchuk <silent_control@tut.by>
2018-01-30 21:35:12 +0200

    Lesson 2

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: "#app",
   data: {
-    string: 'Hello world!'
+    name: 'Yauhen',
+    job: 'Front-end developer'
+  },
+  methods: {
+    greet: function() {
+      return 'Hello me name is ' + this.name;
+    }
   }
 })
