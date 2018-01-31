808fb11907f2b9f14c7b9215caa53a0875c46846
Yauhen_Kavalchuk <silent_control@tut.by>
2018-01-31 18:44:42 +0200

    Lesson 5

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 new Vue({
   el: "#app",
   data: {
-    people: [
-       {
-          name: 'Franky Four Fingers',
-          stillAlive: false
-       },
-       {
-          name: 'Bullet-Tooth Tony',
-          stillAlive: false
-       },
-       {
-          name: 'Turkish',
-          stillAlive: true
-       },
-       {
-          name: 'Boris the Blade',
-          stillAlive: false
-       },
-       {
-          name: 'Mickey',
-          stillAlive: true
-       },
-       {
-          name: 'Brick Top',
-          stillAlive: false
-       }
-    ]
+    counter: 0
+  },
+  methods: {
+    substract: function() {
+      this.counter--;
+    }
   }
 })
