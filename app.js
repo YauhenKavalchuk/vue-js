776aeb274a300fb9adae198a02b039831e8cc436
Yauhen_Kavalchuk <silent_control@tut.by>
2018-01-31 08:26:27 +0200

    Lesson 4

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 new Vue({
   el: "#app",
   data: {
-    link: 'http://www.example.com',
-    markup: '<a href="http://www.example.com">Link</a>',
-    name: 'Jack'
+    people: [
+       {
+          name: 'Franky Four Fingers',
+          stillAlive: false
+       },
+       {
+          name: 'Bullet-Tooth Tony',
+          stillAlive: false
+       },
+       {
+          name: 'Turkish',
+          stillAlive: true
+       },
+       {
+          name: 'Boris the Blade',
+          stillAlive: false
+       },
+       {
+          name: 'Mickey',
+          stillAlive: true
+       },
+       {
+          name: 'Brick Top',
+          stillAlive: false
+       }
+    ]
   }
 })
