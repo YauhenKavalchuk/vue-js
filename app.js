d0899e7e0c0e6f67637c45071e4c85abb5d2e327
Yauhen_Kavalchuk <silent_control@tut.by>
2018-02-01 08:29:55 +0200

    Lesson 9

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 new Vue({
   el: "#app",
   data: {
-    name: '',
-    checked: false
-  },
-  config: {
-
+    a: 0,
+    b: 0,
+    age: 20
   },
   methods: {
 
+  },
+  computed: {
+    addToA: function() {
+      console.log('addToA');
+      return this.a + this.age;
+    },
+    addToB: function() {
+      console.log('addToB');
+      return this.b + this.age;
+    }
   }
 })
