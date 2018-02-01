3272732564ac1d84dc70e6bc19ff9d29786cfb2f
Yauhen_Kavalchuk <silent_control@tut.by>
2018-02-01 19:40:48 +0200

    Lesson 11

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 new Vue({
   el: "#app",
   data: {
-    error: false,
-    success: false
+    currentTask: '',
+    editValue: '',
+    tasks: [
+      {
+        text: 'Subscribe to channel',
+        isCompleted: false,
+        isEditing: false
+      },
+      {
+        text: 'Like the video',
+        isCompleted: false,
+        isEditing: false
+      },
+      {
+        text: 'Learn Vue.js',
+        isCompleted: true,
+        isEditing: false
+      }
+    ]
   },
   methods: {
-
-  },
-  computed: {
-
+    addTask: function() {
+      this.tasks.push({
+        text: this.currentTask,
+        isCompleted: false
+      });
+      this.currentTask = '';
+    },
+    removeTask: function(taskText) {
+      this.tasks = this.tasks.filter(task => {
+        return task.text !== taskText;
+      })
+    },
+    changeEditing: function(taskText) {
+      this.editValue = taskText;
+      this.tasks = this.tasks.map(task => {
+        if(task.text === taskText) {
+          task.isEditing = !task.isEditing;
+        }
+        return task;
+      })
+    },
+    editTask: function(taskText) {
+      this.tasks = this.tasks.map(task => {
+        if(task.text === taskText) {
+          task.isEditing = !task.isEditing;
+          task.text = this.editValue;
+        }
+        return task;
+      })
+    }
   }
 })
