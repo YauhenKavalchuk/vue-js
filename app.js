42c59fc0ef5fc078b80c621b43f54d0d4a925b50
Yauhen_Kavalchuk <silent_control@tut.by>
2018-02-05 08:20:15 +0200

    Lesson 14

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-Vue.component('greet', {
-  template: '<h2>{{title}} <button @click="changeTitle">Change title</button></h2>',
-  data: function() {
-    return {
-      title: 'Title template'
-    }
-  },
-  methods: {
-    changeTitle: function() {
-      this.title = 'Updated title!';
-    }
-  }
-});
-
 new Vue({
   el: "#app-one",
   data: {
-
+    title: 'Title'
   },
   computed: {
 
   },
   methods: {
-
+    changeTitle: function() {
+      //this.title = this.$refs.input.value;
+      console.log(this.$refs);
+    }
   }
 });
